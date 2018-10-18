582995706577150ce9519344be6beb1a21fd3cdb
hizhatt <32994801+hizhatt@users.noreply.github.com>
2018-10-18 22:19:54 +0800

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter the number of rows: ";
+  cin >> n;
+
+// So now we have the number of rows
+// Now we make a loop that print each row
+  for (int i = 1; i <= n; i++) // This will print n rows
+  {
+    for (int j = 1; j <= n-i; j++) // This will print the spaces in each row
+    {
+      cout << " ";
+    }
+
+    for (int k = 1; k <= 2*i-1; k++) // This will print the stars in each row
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
